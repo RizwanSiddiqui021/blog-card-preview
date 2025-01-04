45b7d6d41d9151721ccd256e307bc1567aef54b7
RizwanSiddiqui021 <rizwansiddiqui021@gmail.com>
2025-01-04 19:30:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BlogCard from "./components/BlogCard";
+
+function App() {
+  return (
+    <div className="bg-[#F4D04E] h-screen">
+      <BlogCard></BlogCard>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,9 @@
+const BlogCard = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default BlogCard;
